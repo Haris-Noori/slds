0f243b750025e267e35a80d59f790403bf51e286
Haris <harisnoori20@gmail.com>
2020-11-16 13:36:35 +0500

    UI Changed

--- src/student/my_profile.php
+++ src/student/my_profile.php
@@ -12,32 +12,32 @@
 <?php include "student_head.php"; ?>
     <!-- Operator Team Code Start -->
     <div class="container-fluid">
-        <h2 class="mt-4">My Profile</h2>
+        <h2 class="mt-4"><u>My Profile</u></h2>
 
         <h4 class="mt-4">Enter Your Details</h4>
         <form class="myform" action="" method="POST">
             <div class="form-row">
                 <div class="form-group col-md-4">
                     <label for="">Student Name</label>
-                    <input name="std_name" type="text" class="form-control" id="inputEmail4" placeholder="Full Name">
+                    <input name="std_name" type="text" class="form-control border-dark" id="inputEmail4" placeholder="Full Name">
                 </div>
                 <div class="form-group col-md-4">
                     <label for="">Student Email</label>
-                    <input name="std_email" type="email" class="form-control" id="inputEmail4" placeholder="student@email.com">
+                    <input name="std_email" type="email" class="form-control border-dark" id="inputEmail4" placeholder="student@email.com">
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-2">
                     <label for="">Induction year</label>
-                    <input name="std_ind_year" type="number" class="form-control" id="inputEmail4" placeholder="2020 etc.">
+                    <input name="std_ind_year" type="number" class="form-control border-dark" id="inputEmail4" placeholder="2020 etc.">
                 </div>
-                <div class="form-group col-md-4">
+                <div class="form-group col-md-3">
                     <label for="">Phone Number</label>
-                    <input name="std_phone" type="text" class="form-control" id="inputEmail4" placeholder="123 456 789">
+                    <input name="std_phone" type="text" class="form-control border-dark" id="inputEmail4" placeholder="123 456 789">
                 </div>
                 <div class="form-group col-md-3">
                     <label for="">Academic Program</label>
-                    <input name="std_program" type="text" class="form-control" id="inputEmail4" placeholder="BSCS, BBA, BSIT etc.">
+                    <input name="std_program" type="text" class="form-control border-dark" id="inputEmail4" placeholder="BSCS, BBA, BSIT etc.">
                 </div>
             </div>
             <div class="form-row">
