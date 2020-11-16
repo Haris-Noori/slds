dd8f05beaaed9c1868d28a852eb783b85ac59687
Haris <harisnoori20@gmail.com>
2020-11-16 13:36:55 +0500

    UI Changed

--- src/student/student_login.php
+++ src/student/student_login.php
@@ -11,7 +11,15 @@
     <link rel="stylesheet" href="../../css/bootstrap.min.css">
     <link rel="stylesheet" href="../../css/admin_login.css">
 
-    <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Baloo+Bhaina+2&family=Press+Start+2P&family=Russo+One&display=swap" rel="stylesheet">
+    <!--FONT LINK-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
 </head>
 
 <body>
