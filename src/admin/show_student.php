8f6e73ca5f8e6d8d2bbe5555379e417381982357
Haris <harisnoori20@gmail.com>
2020-11-17 23:35:44 +0500

    [Completed]: Admin - Show Student working

--- src/admin/show_student.php
+++ src/admin/show_student.php
@@ -9,20 +9,20 @@
 </head>
 <body>
     <?php include "admin_header.php" ?>
-        <!-- Admin Dashboard Code Start-->
+        <!-- Admin Show Student Code Start-->
 
             <div class="container-fluid">
                 <h2 class="mt-4"><u>Show Student</u></h2>
                 
-                <form class="myform" action="add_student_try.php" method="POST">
+                <form class="myform" action="" method="POST">
                     <div class="form-row">
                         <div class="form-group col-md-3">
                             <label for="inputEmail4">Student ID</label>
-                            <input type="number" class="form-control border-dark" id="inputEmail4" placeholder="Enter ID" name="" required>
+                            <input type="number" name="std_id" class="form-control border-dark" id="inputEmail4" placeholder="123" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputEmail4"></label>
-                            <button type="submit" class="btn btn-success">Search</button>
+                            <label for="inputEmail4">.</label>
+                            <button type="submit" name="btn-search-std" class="btn form-control btn-success">Search</button>
                         </div>
                     </div>
                     <br>
@@ -33,38 +33,45 @@
 
                     <table class="table mytable">
                         <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Student Name</th>
-                            <th scope="col">Academic Program</th>
-                            <th scope="col">Induction Year</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Phone</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Student Name</th>
+                                <th scope="col">Academic Program</th>
+                                <th scope="col">Induction Year</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Phone</th>
+                            </tr>
                         </thead>
                         <?php
-                        $qry = " SELECT * FROM students ";
-                        $res = $con->query($qry);
-                        $result = "";
-                        if($res->num_rows > 0)
-                        {
-                        echo "Total Students: ".$res->num_rows;
-                        while($row = $res->fetch_assoc())
-                        {
+                            if(isset($_POST["btn-search-std"]))
+                            {
+                                $std_id = $_POST["std_id"];
+                                $qry = " SELECT * FROM student WHERE std_id='".$std_id."' ";
+                                $res = $con->query($qry);
+                                $result = "";
+                                if($res->num_rows > 0)
+                                {
+                                    echo "Total Students: ".$res->num_rows;
+                                    while($row = $res->fetch_assoc())
+                                    {
+
                         ?>
-
                         <tbody>
-                        <tr>
-                            <th scope="row"> <?php echo " ".$row["student_id"]." " ?> </th>
-                            <td><?php echo " ".$row["student_name"]." " ?></td>
-                        </tr>
-
+                            <tr>
+                                <td><?php echo " ".$row["std_id"]." " ?></td>
+                                <td><?php echo " ".$row["std_name"]." " ?></td>
+                                <td><?php echo " ".$row["std_program"]." " ?></td>
+                                <td><?php echo " ".$row["std_ind_year"]." " ?></td>
+                                <td><?php echo " ".$row["std_email"]." " ?></td>
+                                <td><?php echo " ".$row["std_phone"]." " ?></td>
+                            </tr>
                         <?php
-                        }
-                        }
-                        else{echo "No Results Found!!";}
-
-                        //bss yahan tak
+                                    }
+                                }
+                                else{
+                                    echo "No Results Found!!";
+                                }
+                            }
                         ?>
                         </tbody>
                     </table>
