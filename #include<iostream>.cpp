cc17d74593a912557af3142b301fb1d9d4ae9cd7
Haris-Noori <harisnoori20@gmail.com>
2021-08-10 09:30:23 +0500

    checking

---
+++ #include<iostream>.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using name space std;
+int main(){
+    cout<<"Hello world"<<endl;
+    cout<<"Hello change"<<endl;
+    cout<"Hello I have changes"<<endl;
+    cout<<"hey"<<endl;
+    // -----------------------hey----------------------------
+    cout<<"It is confirmed that I have made the changes"<<endl;
+    getch();
+    return 0;
+} 
